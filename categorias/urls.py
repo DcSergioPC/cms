a4ed634d50b7ac35f621a0303449d39174e70e37
DavidAlejandro123 <alejandroariste210@gmail.com>
2024-08-31 16:52:45 -0400

    se corrigio Categorias

--- categorias/urls.py
+++ categorias/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categorias/', views.categoria_list, name='categoria_list'),
-    path('categorias/nueva/', views.categoria_create, name='categoria_create'),
-    path('categorias/<int:pk>/editar/', views.categoria_update, name='categoria_update'),
-    path('categorias/<int:pk>/eliminar/', views.categoria_delete, name='categoria_delete'),
+    path('', views.categoria_list, name='categoria_list'),
+    path('nueva/', views.categoria_create, name='categoria_create'),
+    path('<int:pk>/editar/', views.categoria_update, name='categoria_update'),
+    path('<int:pk>/eliminar/', views.categoria_delete, name='categoria_delete'),
 ]
