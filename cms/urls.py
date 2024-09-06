a27627562cd060d18cfcaa1d821f0f8dda1bd952
DavidAlejandro123 <alejandroariste210@gmail.com>
2024-09-06 10:58:14 -0400

    se corrigio documentacion y urls

--- cms/urls.py
+++ cms/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/cuentas/login/', permanent=True)),
     path('admin/', admin.site.urls),
     path('cuentas/', include('cuentas.urls')),  #
     path('articulos/', include('articulos.urls')),  # 
-    path("accounts/", include("allauth.urls")), #most important
+    path("accounts/", include("allauth.urls")), 
 ] 
 if settings.DEBUG:  # Solo sirve archivos de medios en desarrollo
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
