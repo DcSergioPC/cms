e2143781916bdda1b836c64c46ecf42e3b2ff6ac
DavidAlejandro123 <alejandroariste210@gmail.com>
2024-10-24 00:51:19 -0300

    especificaciones de configuracion de sitio

--- cms/settings.py
+++ cms/settings.py
@@ -213,3 +213,12 @@ USE_TZ = True
 
 #para el login
 SOCIALACCOUNT_STORE_TOKENS = True
+
+#CONFIGURACIONES PARA EL CORREO
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USUARIO')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_CONTRASENA')
+EMAIL_USE_TLS = True
